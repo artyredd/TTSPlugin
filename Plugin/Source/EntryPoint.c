e8989d3faa0ee5c90bbfb2964f968688797297ed
artyredd <canadianspycrab@gmail.com>
2024-06-01 07:02:39 -0400

    generate dll instead of exe

--- Plugin/Source/EntryPoint.c
+++ Plugin/Source/EntryPoint.c
@@ -2,20 +2,20 @@
 
 OnStart(0)
 {
-
+	fprintf(stdout, "Plugin OnStart ran.");
 }
 
 OnUpdate(0)
 {
-
+	fprintf(stdout, "Plugin OnUpdate ran.");
 }
 
 AfterUpdate(0)
 {
-
+	fprintf(stdout, "Plugin AfterUpdate ran.");
 }
 
 OnClose(0)
 {
-
+	fprintf(stdout, "Plugin OnClose ran.");
 }
