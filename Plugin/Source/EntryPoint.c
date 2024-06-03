277fb8dabfc563b4ab0997b3d096d422d0799d52
ArtyRedd <canadianspycrab@gmail.com>
2024-06-03 17:37:59 -0400

    minor

--- Plugin/Source/EntryPoint.c
+++ Plugin/Source/EntryPoint.c
@@ -1,21 +1,25 @@
 #include "core/runtime.h"
+#include "core/tasks.h"
 
 OnStart(1)
 {
-	fprintf(stdout, "Plugin OnStart ran.\n");
+	fprintf_green(stdout, "Plugin OnStart ran: %i\n", Tasks.ThreadId());
 }
+
+int i = 0;
+int x = 0;
 
 OnUpdate(1)
 {
-	fprintf(stdout, "Plugin OnUpdate ran.\n");
+	fprintf_green(stdout, "Plugin OnUpdate ran: %i\n", i++);
 }
 
 AfterUpdate(1)
 {
-	fprintf(stdout, "Plugin AfterUpdate ran.\n");
+	fprintf_green(stdout, "Plugin AfterUpdate ran: %i\n", x++);
 }
 
 OnClose(1)
 {
-	fprintf(stdout, "Plugin OnClose ran.\n");
+	fprintf_green(stdout, "Plugin OnClose ran: %i\n", Tasks.ThreadId());
 }
